07c93f6084d27aa43042bda637a5ffd86bfdaea0
evenrue <kasskat@gmail.com>
2026-02-04 01:05:18 -0800

    mightas well go all the way on the colors

--- timeline/state.js
+++ timeline/state.js
@@ -14,7 +14,16 @@ export const categoryColors = {
     'Open Frequency': [231, 76, 60],           // #e74c3c
     'Mission Briefs (Events)': [46, 204, 113], // #2ecc71
     'Communications': [52, 152, 219],          // #3498db
-    'Reports': [243, 156, 18]                  // #f39c12
+    'Reports': [243, 156, 18],                  // #f39c12
+    'Short Stories': [155, 89, 182],            // #9b59b6
+    'Forum Roleplay': [155, 89, 182], 
+    'Transcripts': [155, 89, 182],    
+    'Art': [155, 89, 182],            
+    'Ship Records': [26, 188, 156],              // #1abc9c
+    'Personnel Records': [26, 188, 156],
+    'Astrometrics': [26, 188, 156],
+    'Organizations': [26, 188, 156],
+    'Miscellaneous': [26, 188, 156]
 };
 
 export function rgba(rgb, a = 1) {
